ff69d0793e236cf83545f27459654cdb1fcae7ba
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-07-30 22:31:44 -0300

    router file created

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const routes = require('./routes');
 
 const app = express();
 mongoose.connect('mongodb+srv://wtn-h29:wtn-h29@cluster0.t2jhc.mongodb.net/wtnh29?retryWrites=true&w=majority', {
