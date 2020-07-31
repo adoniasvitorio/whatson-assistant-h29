f805d0a230be1e0b53e3f713086ee15f2ac4eda3
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-07-30 22:26:09 -0300

    created and connect to mongoDB Databases

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+mongoose.connect('mongodb+srv://wtn-h29:wtn-h29@cluster0.t2jhc.mongodb.net/wtnh29?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
 
 app.get('/', (request, response) =>{
-    return response.json({ message: "hello world !, I'm a bot" });
+    return response.json({ message: "hello world !, I'm assistant bot" });
 });
 
 app.listen(3333);
