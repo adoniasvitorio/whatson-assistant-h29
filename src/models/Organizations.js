1c54291c6b3fa9bba6ea67322ec620725290ed36
Adonias Vitorio <adonias.vitorio@gmail.com>
2020-07-30 23:16:14 -0300

    created organizations schema

---
+++ src/models/Organizations.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const organizationsSchema = new mongoose.Schema({
+    organization_name: String,
+    email: String,
+    avatar_url: String,
+    address: String,
+    address2: String,
+    district: String,
+    city: String,
+    state: String,
+    country: String,
+    postal_code: String,
+    first_phone: String,
+    second_phone: String,
+    connect_phone: String,
+    specialties: [String],
+});
+
+module.exports = mongoos.model('organizations', organizationsSchema);
